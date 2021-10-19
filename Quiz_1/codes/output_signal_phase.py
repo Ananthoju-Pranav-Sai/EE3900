10193c0094c18a4831c8e70b15f8c98fe200a8aa
Ananthoju-Pranav-Sai <ai20btech11004@iith.ac.in>
2021-10-19 14:32:07 +0530

    added Quiz 1

---
+++ Quiz_1/codes/output_signal_phase.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import math
+from matplotlib import pyplot as plt
+
+
+n = np.linspace(0,10,11,endpoint=True)
+
+y=[]
+x=[]
+for i in n:
+    y.append(8*np.e**(1j*np.pi/4)*math.cos(math.pi*(i-1)/2))
+    x.append(math.cos(math.pi*i/2))
+
+plt.stem(n,np.angle(y))
+plt.grid()
+plt.xlabel("n")
+plt.ylabel("arg(y[n])")
+
+plt.show()
